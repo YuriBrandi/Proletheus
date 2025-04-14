6faa769059e91d12323874f8525eab7d0d2733a9
YuriBrandi <destroyercrazy0@gmail.com>
2025-04-15 01:02:38 +0200

    Finished Main Setup

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -1,87 +1,46 @@
 using UnityEngine;
+using UnityEngine.VFX;
 using System.Collections; //Per coroutine
 
 public class CollisionDetection : MonoBehaviour
 {
-    [Tooltip("Set 0 to disable")]
-    public float sphereDiam = 200f; // Radius of the red circle
-    public float animationDuration = .5f;
+    [Tooltip("Set to null to disable")]
     public string missileTag;
-    public Material collisionMat;
+    public VisualEffectAsset explosionEffect;
+    public float explosionSize = 30f;
+    public float vfxLifetime = 5f;
 
-    private Vector3 missilePosition;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Vector3 impactPoint;
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == missileTag)
         {
-            missilePosition = col.transform.position;
+            impactPoint = col.transform.position;
             Destroy(col.gameObject);
-            if (sphereDiam > 0f)
-                DrawHitSphere(missilePosition, sphereDiam);
+            if (explosionEffect != null)
+                SpawnExplosionVFX(impactPoint);
 
             CurriculumDebug.OnEnemyMissileDestroyed(false);
         }
     }
 
-    void DrawHitSphere(Vector3 position, float diameter)
+    void SpawnExplosionVFX(Vector3 position)
     {
-        // Create a new sphere GameObject
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.name = "hitRadiusSphere";
+        GameObject vfxObject = new GameObject("ExplosionVFX");
+        vfxObject.transform.position = position;
+        vfxObject.transform.localScale = Vector3.one * explosionSize;
 
-        Destroy(sphere.GetComponent<Collider>()); // Removes the default SphereCollider
+        VisualEffect vfx = vfxObject.AddComponent<VisualEffect>();
+        vfx.visualEffectAsset = explosionEffect;
 
-        // Set the position of the sphere
-        sphere.transform.position = position; // Centered at origin
-
-        // Set the radius (scale because the default size is 1 unit)
-        //sphere.transform.localScale = Vector3.one * diameter;
-        sphere.transform.localScale = Vector3.zero;
-
-        if (collisionMat != null)
-        {
-            // Apply the material to the sphere
-            Renderer renderer = sphere.GetComponent<Renderer>();
-            renderer.material = collisionMat;
-        }
-        else
-        {
-            Debug.LogError("Material not found!");
-        }
-
-        //Animazione crescente con Coroutine
-        StartCoroutine(AnimazioneSfera(sphere, sphereDiam));
+        // Automatically destroy the VFX object when it finishes
+        StartCoroutine(DestroyVFXAfterDelay(vfxObject, vfxLifetime));
     }
 
-    IEnumerator AnimazioneSfera(GameObject sphere, float targetRadius)
+    IEnumerator DestroyVFXAfterDelay(GameObject vfxObject, float delay)
     {
-        float elapsedTime = 0f; // Tempo
-        Vector3 finalScale = Vector3.one * targetRadius;
-
-        while (elapsedTime < animationDuration)
-        {
-            // Scala la sfera col tempo
-            float t = elapsedTime / animationDuration;
-            sphere.transform.localScale = Vector3.Lerp(Vector3.zero, finalScale, t);
-
-            elapsedTime += Time.deltaTime;
-            yield return null; // Attesa prossimo frame
-        }
-
-        // Elimina margine di approssimazione
-        sphere.transform.localScale = finalScale;
+        yield return new WaitForSeconds(delay);
+        Destroy(vfxObject);
     }
 }
